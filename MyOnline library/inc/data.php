f681306e95c7d9ff0264bfc48d3593bdaaf47f36
Ramsahaisenpai <50430418+Ramsahaisenpai@users.noreply.github.com>
2019-06-03 18:19:54 -0400

    Add files via upload

---
+++ MyOnline library/inc/data.php
@@ -0,0 +1,177 @@
+<?php
+$catalog = [];
+//Books
+$catalog[101] = [
+  "title" => "Ikigami",
+  "img" => "img/media/book1.jpg",
+ "genre" => "Fiction",
+ "format" => "Paperback",
+ "year" => 2005,
+ "category" => "Books",
+ "authors" => [
+     "Motoro Mase",
+ ],
+ "publisher" => "Viz Media",
+ "isbn" => '9780545010221'
+];
+
+$catalog[102] = [
+  "title" => "Mobile Suit Gundam the Origin",
+  "img" => "img/media/book2.jpg",
+ "genre" => "Sci-fi",
+ "format" => "Hardcover",
+ "year" => 2013,
+ "category" => "Books",
+ "authors" => [
+     "Yoshikazu Yasuhiko",
+ ],
+ "publisher" => "Kadokawa Shoten",
+ "isbn" => '9788851127190'
+];
+
+$catalog[103] = [
+  "title" => "Flashpoint",
+  "img" => "img/media/book3.jpg",
+ "genre" => "Fiction",
+ "format" => "Paperback",
+ "year" => 2013,
+ "category" => "Books",
+ "authors" => [
+     "Geoff johns",
+ ],
+ "publisher" => "Dc Comics",
+ "isbn" => '9781408855676'
+];
+
+$catalog[104] = [
+  "title" => "Old man and the sea",
+  "img" => "img/media/book4.jpg",
+ "genre" => "Fiction",
+ "format" => "Paperback",
+ "year" => 1952,
+ "category" => "Books",
+ "authors" => [
+     "Ernest Hemingway",
+ ],
+ "publisher" => "Scribner Reissue edition",
+ "isbn" => '9781449474256'
+];
+
+//Movies
+$catalog[201] = [
+ "title" => "Creed",
+ "img" => "img/media/movie1.jpg",
+ "genre" => "Drama",
+ "format" => "Digital",
+ "year" => 2015,
+ "category" => "Movies",
+ "director" => "Ryan Coogler",
+ "writers" => [
+    "Ryan Coogler"
+
+ ],
+ "stars" => [
+   "Sylvester Stallone",
+   "Michael B. Jordan",
+   "Tessa Thompson"
+ ],
+];
+
+$catalog[202] = [
+ "title" => "Rush Hour",
+ "img" => "img/media/movie2.jpg",
+ "genre" => "Action,Adventure",
+ "format" => "Digital",
+ "year" => 1998,
+ "category" => "Movies",
+ "director" => "Brett Ratner",
+ "writers" => [
+     "Jim Kouf"
+
+ ],
+ "stars" => [
+   "Jackie Chan",
+   "Chris Tucker"
+ ]
+];
+
+$catalog[203] = [
+ "title" => "Logan",
+ "img" => "img/media/movie3.jpg",
+ "genre" => "Action,Adventure",
+ "format" => "Digital",
+ "year" => 2018,
+ "category" => "Movies",
+ "director" => "James Mangold",
+ "writers" => [
+    "James Mangod"
+
+ ],
+ "stars" => [
+   "Hugh Jackman",
+   "Patrick Stewart",
+  "Richard E. Grant",
+ ]
+];
+
+$catalog[204] = [
+ "title" => "Do The Right Thing",
+ "img" => "img/media/movie4.jpg",
+ "genre" => "Drama",
+ "format" => "DVD",
+ "year" => 1988,
+ "category" => "Movies",
+ "director" => "Spike lee",
+ "writers" => [
+      "Spike lee"
+
+ ],
+ "stars" => [
+   "Spike Lee",
+   "Danny Aiello",
+  "Ossie Davis",
+ ]
+];
+
+//Music
+$catalog[301] = [
+ "title" => "The Miseducation of Lauryn Hill",
+ "img" => "img/media/album1.jpg",
+ "genre" => "Pop, indie",
+ "format" => "CD",
+ "year" => 1998,
+ "category" => "Music",
+ "artist" => "Lauryn Hill"
+];
+
+$catalog[302] = [
+ "title" => "Midnight Marauders",
+ "img" => "img/media/album2.jpg",
+ "genre" => "Pop",
+ "format" => "CD",
+ "year" => 1993,
+ "category" => "Music",
+ "artist" => "A Tribe Called Quest"
+];
+
+$catalog[303] = [
+ "title" => "Vivid",
+ "img" => "img/media/album3.jpg",
+ "genre" => "Pop, indie",
+ "format" => "CD",
+ "year" => 1988,
+ "category" => "Music",
+ "artist" => "Living Colour"
+];
+
+$catalog[304] = [
+ "title" => "Cooleyhighharmony",
+ "img" => "img/media/album4.jpg",
+ "genre" => "Pop",
+ "format" => "CD",
+ "year" => 1991,
+ "category" => "Music",
+ "artist" => "Boyz II Men"
+];
+
+?>
